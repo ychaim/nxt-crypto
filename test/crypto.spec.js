e06db80d860b17f4f3a68dc072be82e061d401c1
Martijn Buurman <martijn@m19.nl>
2017-01-13 16:17:23 +0800

    Added test

--- test/crypto.spec.js
+++ test/crypto.spec.js
@@ -45,6 +45,10 @@ describe('nxtCrypto', () => {
     it('should return the correct account reed solomon', () => {
       expect(getAccountRS(publicKey)).to.equal('NXT-E8JD-FHKJ-CQ9H-5KGMQ')
     })
+
+    it('should return the correct account reed solomon with a different prefix', () => {
+      expect(getAccountRS(publicKey, 'DBN')).to.equal('DBN-E8JD-FHKJ-CQ9H-5KGMQ')
+    })
   })
 
   describe('#getAccountRSFromSecretPhrase', () => {
